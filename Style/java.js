0960de0de1c2bea175a851b00d61e7f384d82b23
Gauravved <gaurav.rpandit1@gmail.com>
2021-10-04 18:41:41 +0530

    Added a Javascript file and BMI calculator logic

--- Style/java.js
+++ Style/java.js
@@ -1,5 +1,7 @@
-var bmi=0
 
+function Functions() {
+    let bmii=myFunction();
+}
 function myFunction() {
     var height = parseInt(document.querySelector('#height').value);
     var weight = parseFloat(document.querySelector('#weight').value);
@@ -9,22 +11,19 @@ function myFunction() {
     else if (weight === "" || isNaN(weight))
         alert('Invalid weight');
     else {
-        window.bmi = (weight / ((height / 100) * (height / 100))).toFixed(2);
+        let bmi = (weight / ((height / 100) * (height / 100))).toFixed(2);
         if (bmi < 18.5) {
-            alert('You are under Weight. BMI:' + window.bmi)
+            alert('You are under Weight. BMI:' + bmi)
         }
         else if (bmi >= 18.5 && bmi < 24.9) {
-            alert('Your weight is ideal. BMI:' + window.bmi)
+            alert('Your weight is ideal. BMI:' + bmi)
         }
         else if (bmi > 25 && bmi < 29.9) {
-            alert('You are over-weight. BMI:' + window.bmi)
+            alert('You are over-weight. BMI:' + bmi)
         }
         else if (bmi > 30) {
-            alert('Obese Weight. BMI:' + window.bmi);
+            alert('Obese Weight. BMI:' + bmi);
         }
+        return bmi;
     }
-}
-
-function func() {
-    alert("BMI is:" + window.bmi);
 }
